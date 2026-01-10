a252be259e26cba53552797da6c2da27149ce6d9
Rahul Raj <rr7308241@gmail.com>
2026-01-10 08:53:25 +0530

    Created UI with Tailwind

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <>
+        <div className='my-5'>
+        <h1 className='text-2xl font-bold text-gray-800 mb-4 text-center'>CodeRoaster AI 🔥</h1>
+        <p className="text-gray-600 mb-4 text-center">
+          Paste your bad code below and let the AI judge you.
+        </p>
+        <div className='flex flex-col gap-4 items-center justify-center w-full px-4'>
+            <textarea className='border-2 border-gray-300 rounded p-2 h-64 w-full max-w-lg' placeholder="Paste your spaghetti code here..."></textarea>
+            <button className='bg-sky-300 hover:bg-sky-700 hover:text-white rounded py-2 px-2 font-bold text-xl cursor-pointer'>Roast My Code</button>
+        </div>
+        </div>
+        </>
+        )
+    }
+export default Home
