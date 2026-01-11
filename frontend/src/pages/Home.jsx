9533205cae7b05b8e9d5df742e671b6165aa7771
Rahul Raj <rr7308241@gmail.com>
2026-01-11 09:14:37 +0530

    UI done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,6 @@
+import {useState} from 'react'
 const Home = () => {
+    const [code, setCode] = useState('')
     return (
         <>
         <div className='my-5'>
@@ -7,7 +9,7 @@ const Home = () => {
           Paste your bad code below and let the AI judge you.
         </p>
         <div className='flex flex-col gap-4 items-center justify-center w-full px-4'>
-            <textarea className='border-2 border-gray-300 rounded p-2 h-64 w-full max-w-lg' placeholder="Paste your spaghetti code here..."></textarea>
+            <textarea className='border-2 border-gray-300 rounded p-2 h-64 w-full max-w-lg' placeholder="Paste your spaghetti code here..." value={code} onChange={(e) => setCode(e.target.value)}></textarea>
             <button className='bg-sky-300 hover:bg-sky-700 hover:text-white rounded py-2 px-2 font-bold text-xl cursor-pointer'>Roast My Code</button>
         </div>
         </div>
